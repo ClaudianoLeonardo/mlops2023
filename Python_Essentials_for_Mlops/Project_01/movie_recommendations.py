751cf843ccca0f26e7b4815a857285e80637d7eb
Claudiano <55771749+ClaudianoLeonardo@users.noreply.github.com>
2023-10-08 21:05:06 +0000

    improvements project01

--- Python_Essentials_for_Mlops/Project_01/movie_recommendations.py
+++ Python_Essentials_for_Mlops/Project_01/movie_recommendations.py
@@ -1,105 +1,68 @@
-import pandas as pd
+"""
+Movie Recommendation System
 
-# https://files.grouplens.org/datasets/movielens/ml-25m.zip
-movies = pd.read_csv("movies.csv")
-
-movies.head()
-
+This script provides a simple movie recommendation system based on user input.
+"""
 import re
-
-def clean_title(title):
-    title = re.sub("[^a-zA-Z0-9 ]", "", title)
-    return title
-
-movies["clean_title"] = movies["title"].apply(clean_title)
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer(ngram_range=(1,2))
-
-tfidf = vectorizer.fit_transform(movies["clean_title"])
-
-
-from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-
-def search(title):
-    title = clean_title(title)
-    query_vec = vectorizer.transform([title])
-    similarity = cosine_similarity(query_vec, tfidf).flatten()
-    indices = np.argpartition(similarity, -5)[-5:]
-    results = movies.iloc[indices].iloc[::-1]
-    
-    return results
-
+import pandas as pd
 import ipywidgets as widgets
 from IPython.display import display
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-movie_input = widgets.Text(
-    value='Toy Story',
-    description='Movie Title:',
-    disabled=False
-)
-movie_list = widgets.Output()
+def clean_movie_title(title):
+    """
+    Remove caracteres não alfanuméricos do título do filme.
 
-def on_type(data):
-    with movie_list:
-        movie_list.clear_output()
-        title = data["new"]
-        if len(title) > 5:
-            display(search(title))
+    Args:
+        title (str): O título do filme.
 
-movie_input.observe(on_type, names='value')
-
-
-display(movie_input, movie_list)
-
-
-movie_id = 89745
-
-#def find_similar_movies(movie_id):
-movie = movies[movies["movieId"] == movie_id]
-
-
-ratings = pd.read_csv("ratings.csv")
-
-similar_users = ratings[(ratings["movieId"] == movie_id) & (ratings["rating"] > 4)]["userId"].unique()
-similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) & (ratings["rating"] > 4)]["movieId"]
-similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
-
-similar_user_recs = similar_user_recs[similar_user_recs > .10]
-all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) & (ratings["rating"] > 4)]
-all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
-rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
-rec_percentages.columns = ["similar", "all"]
-rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
-rec_percentages = rec_percentages.sort_values("score", ascending=False)
-rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")
+    Returns:
+        str: O título do filme após a limpeza.
+    """
+    return re.sub("[^a-zA-Z0-9 ]", "", title)
 
 def find_similar_movies(movie_id):
-    similar_users = ratings[(ratings["movieId"] == movie_id) & (ratings["rating"] > 4)]["userId"].unique()
-    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) & (ratings["rating"] > 4)]["movieId"]
+    """
+    Encontra filmes similares com base nas avaliações de usuários.
+
+    Args:
+        movie_id (int): O ID do filme para o qual deseja encontrar recomendações.
+
+    Returns:
+        pandas.DataFrame: Um DataFrame com as principais recomendações de filmes.
+    """
+
+    similar_users = ratings[(ratings["movieId"] == movie_id) &
+                            (ratings["rating"] > 4)]["userId"].unique()
+    similar_user_recs = ratings[(ratings["userId"].isin(similar_users)) &
+                                (ratings["rating"] > 4)]["movieId"]
     similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
 
-    similar_user_recs = similar_user_recs[similar_user_recs > .10]
-    all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) & (ratings["rating"] > 4)]
+    similar_user_recs = similar_user_recs[similar_user_recs > 0.10]
+    all_users = ratings[(ratings["movieId"].isin(similar_user_recs.index)) &
+                        (ratings["rating"] > 4)]
     all_user_recs = all_users["movieId"].value_counts() / len(all_users["userId"].unique())
     rec_percentages = pd.concat([similar_user_recs, all_user_recs], axis=1)
     rec_percentages.columns = ["similar", "all"]
-    
+
     rec_percentages["score"] = rec_percentages["similar"] / rec_percentages["all"]
     rec_percentages = rec_percentages.sort_values("score", ascending=False)
-    return rec_percentages.head(10).merge(movies, left_index=True, right_on="movieId")[["score", "title", "genres"]]
-import ipywidgets as widgets
-from IPython.display import display
-
-movie_name_input = widgets.Text(
-    value='Toy Story',
-    description='Movie Title:',
-    disabled=False
+    return (
+    rec_percentages.head(10)
+    .merge(movies, left_index=True, right_on="movieId")
+    [["score", "title", "genres"]]
 )
-recommendation_list = widgets.Output()
 
-def on_type(data):
+
+def on_type_recommendations(data):
+    """
+    Atualiza a lista de recomendações com base na entrada do usuário.
+
+    Args:
+        data (ipywidgets.WidgetEvent): O evento de mudança de valor do widget de entrada de filme.
+    """
     with recommendation_list:
         recommendation_list.clear_output()
         title = data["new"]
@@ -108,6 +71,43 @@ def on_type(data):
             movie_id = results.iloc[0]["movieId"]
             display(find_similar_movies(movie_id))
 
-movie_name_input.observe(on_type, names='value')
+def search(title):
+    """
+    Realiza uma pesquisa de filmes com base no título fornecido.
 
+    Args:
+        title (str): O título do filme a ser pesquisado.
+
+    Returns:
+        pandas.DataFrame: Um DataFrame com os resultados da pesquisa.
+    """
+    title = clean_movie_title(title)
+    query_vec = vectorizer.transform([title])
+    similarity = cosine_similarity(query_vec, tfidf).flatten()
+    indices = np.argpartition(similarity, -5)[-5:]
+    results = movies.iloc[indices].iloc[::-1]
+    return results
+
+# Carregar dados
+movies = pd.read_csv("movies.csv")
+ratings = pd.read_csv("ratings.csv")
+
+# Configurar widget de entrada de título de filme
+movie_name_input = widgets.Text(
+    value='Toy Story',
+    description='Movie Title:',
+    disabled=False
+)
+recommendation_list = widgets.Output()
+
+# Observar a entrada do usuário para atualizar a lista de recomendações
+movie_name_input.observe(on_type_recommendations, names='value')
+
+# Exibir o widget de entrada e a lista de recomendações
 display(movie_name_input, recommendation_list)
+
+# Configurar vetorizador TF-IDF
+vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+
+# Calcular TF-IDF para os títulos de filmes
+tfidf = vectorizer.fit_transform(movies["title"])
